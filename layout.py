44dfeabdfdfc6c821188e432714b2bd9995cb63d
dbehdrnr0202 <50646885+dbehdrnr0202@users.noreply.github.com>
2024-04-23 15:41:08 +0900

    feat: testing some layout

--- layout.py
+++ layout.py
@@ -1,69 +1,52 @@
 import streamlit as st # type: ignore
 from PIL import Image # type: ignore
 import pandas as pd # type: ignore
-
+import map
 def create_layout():
+    # 공간을 2:3 으로 분할 후, col1과 col2라는 이름을 가진 컬럼을 생성합니다.  
     col1,col2 = st.columns([2,3])
-    # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.  
 
     with col1 :
-    # column 1 에 담을 내용
         st.title('here is column1')
     with col2 :
-    # column 2 에 담을 내용
         st.title('here is column2')
         st.checkbox('this is checkbox1 in col2 ')
-    # with 구문 말고 다르게 사용 가능 
     col1.subheader(' i am column1  subheader !! ')
     col2.checkbox('this is checkbox2 in col2 ') 
-
+    map.print_map()
 def create_tab():
-    # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다. 
-    tab1, tab2= st.tabs(['Tab A' , 'Tab B'])
+    tab1, tab2= st.tabs(['Tab1' , 'Tab2'])
     with tab1:
-    #tab A 를 누르면 표시될 내용
-        st.write('hello')
+        st.write('msg_tab_1')
     with tab2:
-    #tab B를 누르면 표시될 내용 
-        st.write('hi')
+        st.write('msg_tab_2')
         
 def create_sidebar(origin_df:pd.DataFrame, columns:list[str]):
     st.sidebar.title('sidebar')
     st.sidebar.checkbox('pi chart')
     st.sidebar.checkbox('heatmap')
-    
     # 사이드바에 체크박스, 버튼등 추가할 수 있습니다! 
-    # 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
+    # 사이드바에 select box를 활용하여 차트 출력하기
     st.sidebar.title('제주도 여행~')
 
-    # select_species 변수에 사용자가 선택한 값이 지정됩니다
-    selected_column = st.sidebar.selectbox(
-        '관련 컬럼을 확인하세요',
-        columns
-    )
-    # 원래 dataframe으로 부터 꽃의 종류가 선택한 종류들만 필터링 되어서 나오게 일시적인 dataframe을 생성합니다
+    # selected_column 변수에 사용자가 선택한 값이 지정됩니다
+    selected_column = st.sidebar.selectbox('관련 컬럼을 확인하세요',columns)
+    
     tmp_df = origin_df[selected_column]
-    # df[df['species']== select_species]
-    # 선택한 종의 맨 처음 5행을 보여줍니다 
     st.table(tmp_df.head())
     
-    
-    select_multi_species = st.sidebar.multiselect(
-    '확인하고자 하는 종을 선택해 주세요. 복수선택가능',
-    ['setosa','versicolor','virginica']
+    selected_multi_columns = st.sidebar.multiselect('확인하고자 하는 컬럼들을 선택해 주세요. 복수선택가능',columns)
 
-    )
-
-    # 원래 dataframe으로 부터 꽃의 종류가 선택한 종류들만 필터링 되어서 나오게 일시적인 dataframe을 생성합니다
     tmp_df = []
-    # df[df['species'].isin(select_multi_species)]
-    # 선택한 종들의 결과표를 나타냅니다.  
     st.table(tmp_df)
-    
-def load_img():
+
+def load_img(file_name:str):
     #PIL 패키지에 이미지 모듈을 통해 이미지 열기 
     # Image.open('이미지 경로')
-    zarathu_img = Image.open('zarathu.png')
+    try:
+        opened_img = Image.open(file_name)
+    except FileNotFoundError as e:
+        print(e)
     col1,col2 = st.columns([2,3])
     with col1 :
     # column 1 에 담을 내용
@@ -73,4 +56,35 @@ def load_img():
         st.title('here is column2')
         st.checkbox('this is checkbox1 in col2 ')
     # 컬럼2에 불러온 사진 표시하기
-    col2.image(zarathu_img)
+    col2.image(opened_img)
+    
+def print_slider(df, select_multi_species, columns):
+    # 라디오에 선택한 내용을 radio select변수에 담습니다
+    radio_select =st.sidebar.radio(
+        "what is key column?",
+        columns,
+        horizontal=True
+        )
+    # 선택한 컬럼의 값의 범위를 지정할 수 있는 slider를 만듭니다. 
+    slider_range = st.sidebar.slider(
+        "choose range of key column",
+        0.0, #시작 값 
+        10.0, #끝 값  
+        (2.5, 7.5) # 기본값, 앞 뒤로 2개 설정 /  하나만 하는 경우 value=2.5 이런 식으로 설정가능
+    )
+
+    # 필터 적용버튼 생성 
+    start_button = st.sidebar.button(
+        "apply filter"
+    )
+
+    # button이 눌리는 경우 start_button의 값이 true로 바뀌게 된다.
+    # 이를 이용해서 if문으로 버튼이 눌렸을 때를 구현 
+    if start_button:
+        tmp_df = df[df['species'].isin(select_multi_species)]
+        #slider input으로 받은 값에 해당하는 값을 기준으로 데이터를 필터링합니다.
+        tmp_df= tmp_df[ (tmp_df[radio_select] >= slider_range[0]) & (tmp_df[radio_select] <= slider_range[1])]
+        st.table(tmp_df)
+        # 성공문구 + 풍선이 날리는 특수효과 
+        st.sidebar.success("Filter Applied!")
+        st.balloons()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd # type: ignore
 import numpy as np # type: ignore
 import pydeck as pdk # type: ignore
 import layout
-
+import map
 # 기본 형식
 def main():
     # 화면에 관해 처리를 하고 싶다면 st를 사용해야한다.
@@ -14,67 +14,7 @@ def main():
     st.markdown('**아름다운 섬 제주**, 어디로 놀러가면 좋을까?')
     layout.create_layout()
     layout.create_sidebar([], ['a', 'b'])
-    layout.create_tab()
+    layout.create_tab()    
     
 if __name__ == '__main__':
     main()
-
-# number1
-
-st.text('첫번째 지도')
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [33.4, 126.8],
-    columns=['lat', 'lon'])
-
-st.map(df)
-
-st.text('두번째 지도')
-# 점의 크기와 색상을 지정
-st.map(df, size=20, color='#0044ff')
-
-st.text('세번째 지도')
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
-
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    color='col4')
-
-chart_data = pd.DataFrame(
-   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-   columns=['lat', 'lon'])
-
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'hexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
