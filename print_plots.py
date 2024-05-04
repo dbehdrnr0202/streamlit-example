294ae57f6d1bc0bb9e8ad6b89199570cc3b1106c
ehdmso <admin@Adminui-MacBookAir-2.local>
2024-05-04 18:21:15 +0900

    add heatmap plot

--- print_plots.py
+++ print_plots.py
@@ -32,3 +32,23 @@ def print_pie_chart(df:pd.DataFrame, column:str, hole_size:float=0):
                   marker=dict(line=dict(color='#000000', width=2)), hole=hole_size)
     # fig.show()
     return fig
+
+## 도은추가
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+matplotlib.rcParams['font.family'] = 'AppleGothic'
+
+def print_twoway_heatmap(df:pd.DataFrame, columns:list[str]):
+    # columns의 빈도 계산
+    column1, column2 = columns[0], columns[1]
+    freq_df = df.groupby([column1,column2]).size().unstack(fill_value=0)
+
+    # 그래프 생성
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(freq_df, annot=True, cmap='Blues', fmt='.2f')
+    plt.title(f"Frequency by {column1} and {column2}")
+    plt.xlabel(f"{column1}")
+    plt.ylabel(f"{column2}")
+
+    return plt.gcf()
