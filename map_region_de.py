ee9358af0998e211c02959a861e7b77a1d36415b
ehdmso <admin@Adminui-MacBookAir-2.local>
2024-05-05 18:49:02 +0900

    upgrade dictionary 행정구역

--- map_region_de.py
+++ map_region_de.py
@@ -22,7 +22,14 @@ dic_addr = {'화북일동':'화북동', '화북이동':'화북동',
             '오라일동':'오라동', '오라이동':'오라동', '오라삼동':'오라동',
             '외도일동':'외도동', '외도이동':'외도동',
             '이호일동':'이호동', '이호이동':'이호동', 
-            '도두일동':'도두동', '도두이동':'도두동'
+            '도두일동':'도두동', '도두이동':'도두동',
+            '법환동':'대륜동','서호동':'대륜동','호근동':'대륜동',
+            '강정동':'대천동','영남동':'대천동','월평동':'대천동','하원동':'대천동',
+            '서귀동':'송산동','보목동':'송산동',
+            '상효동':'영천동','토평동':'영천동',
+            '색달동':'예래동','하예동':'예래동',
+            '신효동':'효돈동','하효동':'효돈동',
+            '대포동':'중문동','도순동':'중문동','상예동':'중문동','회수동':'중문동'
             }
 df['adm_nm'] = df['adm_nm'].replace(dic_addr, regex=True)
 
@@ -43,22 +50,21 @@ m = folium.Map(location=[33.3617, 126.5292], zoom_start=10)
 # 색상을 동적으로 조정하는 함수
 def style_function(feature):
     count = feature['properties']['visit_counts']
+    print('this is count', count)
+    if count is None:
+        fillColor = 'black'
+    else:
+        intensity = max(0, 255 - int(min(count, 100) * 2.55))
+        fillColor = f'#00{intensity:02x}00'
+    
+    
     return {
-        'fillColor': '#green' if count is None else f'#{int(255 * (1 - min(count, 10)/10)):02x}ff00',
+        'fillColor': fillColor,
         'color': 'black',
         'weight': 2,
         'dashArray': '5, 5',
         'fillOpacity': 0.6
     }
-
-# # Function to determine style (optional customization)
-# def style_function(feature):
-#     return {
-#         'fillColor': 'green',
-#         'color': 'black',
-#         'weight': 2,
-#         'dashArray': '5, 5'
-#     }
 
 # Add the GeoJSON overlay to the map
 folium.GeoJson(
@@ -68,7 +74,7 @@ folium.GeoJson(
     tooltip=folium.GeoJsonTooltip(fields=['adm_nm'], labels=True)  # Using 'adm_nm' as the field name
 ).add_to(m)
 
-print(jeju_map[['adm_nm','visit_counts']])
+# print(jeju_map[['adm_nm','visit_counts']])
 
 # Display the map in Streamlit
 st_data = st_folium(m, width=725, height=500)
